3d919ff70c6fdf6c1e03aaedfb0eee4ce051c7b5
Jack Duignan <jackpduignan@gmail.com>
2025-01-05 11:18:35 +1300

    uptime module working

---
+++ avr_extends/uptime.h
@@ -0,0 +1,34 @@
+/** 
+ * @file uptime.h
+ * @author Jack Duignan (JackpDuignan@gmail.com)
+ * @date 2025-01-05
+ * @brief Allows checking of the up time of the processor
+ * 
+ * @note uses TIMER1B
+ */
+
+
+#ifndef UPTIME_H
+#define UPTIME_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+ISR(TIMER1_COMPA_vect);
+
+/** 
+ * @brief Initialise the uptime module setting up the required timer 1B
+ * 
+ */
+void uptime_init(void);
+
+/** 
+ * @brief Get the current elapsed time since start
+ * 
+ * @return the elapsed time in ms
+ */
+uint64_t uptime_ms(void);
+
+
+#endif // UPTIME_H
---
+++ uptime/uptime.c
@@ -0,0 +1,44 @@
+/** 
+ * @file uptime.c
+ * @author Jack Duignan (JackpDuignan@gmail.com)
+ * @date 2025-01-05
+ * @brief Implementation for the uptime module to store the time since initialisation
+ * 
+ * @cite https://github.com/monoclecat/avr-millis-function/
+ */
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/atomic.h>
+
+#include "avr_extends/GPIO.h"
+
+volatile uint64_t timerCount = 0; // The uptime count
+
+ISR(TIMER1_COMPA_vect) {
+    timerCount++;
+}
+
+void uptime_init(void) {
+    uint16_t msCount = ((F_CPU / 1000) / 8); // When count is this 1ms passed
+
+    TCCR1B |= (1 << WGM12) | (1 << CS11); // Count Up, clk/8 prescaler 
+
+    OCR1A = msCount;
+    
+    TIMSK1 |= (1 << OCIE1A);
+}
+
+ uint64_t uptime_ms(void) {
+    uint64_t timerCountReturn = 0;
+
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+        timerCountReturn = timerCount;
+    }
+
+    return timerCountReturn;
+ }
